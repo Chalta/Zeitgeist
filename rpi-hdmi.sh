a9238d4ab7bb4435d3c7165ba32ed0d577610e5d
Chalta <bills@fiss.ca>
2017-02-15 13:07:01 -0700

    Update rpi-hdmi.sh

--- rpi-hdmi.sh
+++ rpi-hdmi.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+#---------------------------------------
+# Description
+#---------------------------------------
+
 # Enable and disable HDMI output on the Raspberry Pi
 # Original code by AGWA: https://gist.github.com/AGWA/9874925
+
+#---------------------------------------
+# Instructions
+#---------------------------------------
+
+#Instructions: Put this script in /home/pi/rpi-hdmi.sh
+#Then, make it executable: chmod +x /home/pi/rpi-hdmi.sh
+#Then update cron tab with the optional cron entries in this repository.
+
+#---------------------------------------
+# Code
+#---------------------------------------
+
 
 #Basic function to check if the tvservice (HDMI port) is currently off.
 is_off ()
@@ -9,10 +26,10 @@ is_off ()
 	tvservice -s | grep "TV is off" >/dev/null
 }
 
-#Script to turn the tvservice on, off or determine current status.
+#Script to turn the tvservice on, off and/or determine current status.
 case $1 in
 
-	# Shell script was called with request to turn HDMI service off. (i.e. The first argument, $1, is "off")
+	# The shell script was called with request to turn HDMI service off. (i.e. The first argument, $1, is "off")
 	off)
 		#So do it. Turn it off.
 		tvservice -o
@@ -23,6 +40,7 @@ case $1 in
 		#So if it is off, turn it on.
 		if is_off
 		then
+			#Reinitialize X11
 			tvservice -p
 			curr_vt=`fgconsole`
 			if [ "$curr_vt" = "1" ]
@@ -47,6 +65,7 @@ case $1 in
 		fi
 	;;
 	*)
+	#If no on|off|status argument was specified, provide basic instruction for usage of this script
 		echo "Usage: $0 on|off|status" >&2
 		exit 2
 	;;
