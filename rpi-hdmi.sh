f26bba3b24b39f44341cd2491d49639970fa75c0
Chalta <bills@fiss.ca>
2017-02-15 13:02:35 -0700

    Update rpi-hdmi.sh
    Instructions: Put this script in /home/pi/rpi-hdmi.sh
    Then, make it executable: chmod +x /home/pi/rpi-hdmi.sh
    Then update cron tab with the optional cron entries in this repository.

--- rpi-hdmi.sh
+++ rpi-hdmi.sh
@@ -3,16 +3,24 @@
 # Enable and disable HDMI output on the Raspberry Pi
 # Original code by AGWA: https://gist.github.com/AGWA/9874925
 
+#Basic function to check if the tvservice (HDMI port) is currently off.
 is_off ()
 {
 	tvservice -s | grep "TV is off" >/dev/null
 }
 
+#Script to turn the tvservice on, off or determine current status.
 case $1 in
+
+	# Shell script was called with request to turn HDMI service off. (i.e. The first argument, $1, is "off")
 	off)
+		#So do it. Turn it off.
 		tvservice -o
 	;;
+	
+	# Shell script was called with request to turn HDMI service on.	
 	on)
+		#So if it is off, turn it on.
 		if is_off
 		then
 			tvservice -p
@@ -27,7 +35,10 @@ case $1 in
 			fi
 		fi
 	;;
+
+	# Shell script was called with request for current status	
 	status)
+		#So announce the current status of the HDMI service.
 		if is_off
 		then
 			echo off
