94f35c1a23a7aceb8f19afc9fe118de31480e38a
Chalta <bills@fiss.ca>
2017-02-15 13:44:17 -0700

    Create PCOlive.sh

---
+++ PCOlive.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#---------------------------------------
+# Description
+#---------------------------------------
+
+# Launch PCO Live in a Chromium web browser.
+# Will show the next or currently live service for a specified service type, provided as an argument in the crontab.
+
+#---------------------------------------
+# Instructions
+#---------------------------------------
+
+#Instructions: Put this script in /home/pi/PCOlive.sh
+#Then, make it executable: chmod +x /home/pi/rpi-hdmi.sh
+#Then update cron tab with the optional cron entries in this repository: https://github.com/Chalta/Zeitgeist/blob/master/cron
+
+#---------------------------------------
+# Code
+#---------------------------------------
+
+
+#Operate on the first display on the local machine
+DISPLAY=:0
+
+#returns the first line (head- 1) in a search for the window ID of the Chromium browser.
+WID=$(xdotool search --onlyvisible --class chromium|head -1)
+
+#Opens the address bar of Chromium, types a URL and presses enter
+xdotool windowactivate ${WID}
+xdotool key ctrl+l
+xdotool type 'https://services.planningcenteronline.com/service_types/$1/plans/after/today/live'
+xdotool key Return
+
+
+
