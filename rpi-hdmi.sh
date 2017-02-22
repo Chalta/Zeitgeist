253c1a0a0490ae12339a7cd66451a8767134f5bc
Chalta <bills@fiss.ca>
2017-02-22 10:17:57 -0700

    Update rpi-hdmi.sh

--- rpi-hdmi.sh
+++ rpi-hdmi.sh
@@ -12,8 +12,6 @@
 #---------------------------------------
 
 #Instructions: Put this script in /home/pi/rpi-hdmi.sh
-#Then, make it executable: chmod +x /home/pi/rpi-hdmi.sh
-#Then update cron tab with the optional cron entries in this repository: https://github.com/Chalta/Zeitgeist/blob/master/cron
 
 #---------------------------------------
 # Code
